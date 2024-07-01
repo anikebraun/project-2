31bbef3b5b73ff640062a1a26d23b5813146199a
Alice Smith <alice@webage.com>
2024-07-01 14:52:27 -0400

    creating routes upper6bullets

--- swapi-server/server.js
+++ swapi-server/server.js
@@ -15,16 +15,100 @@ const collectionFilmsPlanets = "films_planets";
 const app = express();
 const PORT = 3000;
 
+//PLANETS
 app.get("/api/planets", async (req, res) => {
   try {
     const client = await MongoClient.connect(url);
     const db = client.db(dbName);
-    const collection = db.collection(collectionPlanets);
+    const collection = db.collection(collPlans);
     const planets = await collection.find({}).toArray();
     res.json(planets);
   } catch (err) {
     console.error("Error:", err);
+    res.status(500).send("no planet found");
+  }
+});
+
+//CHARACTERS
+app.get("/api/characters", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collChars);
+    const characters = await collection.find({}).toArray();
+    res.json(characters);
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .send("no character found");
+  }
+});
+
+
+//FILMS
+app.get("/api/films", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collFilms);
+    const films = await collection.find({}).toArray();
+    res.json(films);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("no film found");
+  }
+});
+
+//CHARACTERS/:id
+app.get("/api/characters/:id", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionCharacters);
+    const characters = await collection.find({}).toArray();
+    res.json(characters);
+  } catch (err) {
+    console.error("Error:", err);
     res.status(500).send("error");
+  }
+});
+
+//FILMS/:id
+app.get("/api/films/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collFilms);
+    const result = await collection.findOne({ id: id });
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No film found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).send("no film found");
+  }
+});
+
+//PLANETS/:id
+app.get("/api/planets/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collPlans);
+    const result = await collection.findOne({ id: id });
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send("No planet found");
+    }
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).send("no planet found");
   }
 });
 
