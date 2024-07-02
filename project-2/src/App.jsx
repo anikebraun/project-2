b9091a85ef9cd3b8b50335acbc2b19298c5925e2
Alice Smith <alice@webage.com>
2024-07-02 11:55:45 -0400

    appjsx

---
+++ project-2/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Home.jsx";
+
+
+function App() {
+  const [data, setData] = useState([]);
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+         
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
